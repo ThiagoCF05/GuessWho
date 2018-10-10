df93de0cf415485a2cbcfc0d2c8fd322d6e69913
Thiago Castro Ferreira <thiago.castro.ferreira@gmail.com>
2018-10-10 10:53:05 +0200

    changes on the parsing

--- stanford.py
+++ stanford.py
@@ -13,24 +13,26 @@ if __name__ == '__main__':
     for guess in guesses:
         if guess['actionType'] == 'questionAsked':
             out = proc.parse_doc(guess['question'].replace('[comma]', ','))
+            guess['nlp'] = {}
             if len(out['sentences']) > 0:
-                guess['tokens'] = out['sentences'][0]['tokens']
-                guess['pos_tag'] = out['sentences'][0]['pos']
-                guess['parse'] = out['sentences'][0]['parse']
+                guess['nlp']['tokens'] = out['sentences'][0]['tokens']
+                guess['nlp']['pos_tag'] = out['sentences'][0]['pos']
+                guess['nlp']['parse'] = out['sentences'][0]['parse']
             else:
-                guess['tokens'] = []
-                guess['pos_tag'] = []
-                guess['parse'] = ''
+                guess['nlp']['tokens'] = []
+                guess['nlp']['pos_tag'] = []
+                guess['nlp']['parse'] = ''
         else:
-            guess['tokens'] = []
-            guess['pos_tag'] = []
-            guess['parse'] = ''
+            guess['nlp']['tokens'] = []
+            guess['nlp']['pos_tag'] = []
+            guess['nlp']['parse'] = ''
 
     for face in faces:
         out = proc.parse_doc(face['responses']['description'].replace('[comma]', ','))
-        face['tokens'] = out['sentences'][0]['tokens']
-        face['pos_tag'] = out['sentences'][0]['pos']
-        face['parse'] = out['sentences'][0]['parse']
+        face['nlp'] = {}
+        face['nlp']['tokens'] = out['sentences'][0]['tokens']
+        face['nlp']['pos_tag'] = out['sentences'][0]['pos']
+        face['nlp']['parse'] = out['sentences'][0]['parse']
 
     json.dump(guesses, open('../Guess-who-dataset/guess-who-data.json', 'w'), indent=4, separators=(',', ': '))
     json.dump(faces, open('../Guess-who-dataset/face-rating-data.json', 'w'), indent=4, separators=(',', ': '))
